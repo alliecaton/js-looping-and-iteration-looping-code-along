8337454e4941bbae6be0f0fd9f539ec7941bd8d8
Allie <alliecaton3@gmail.com>
2021-03-15 13:54:15 -0400

    commit tree 1a0ded0d1bbf020f68d1d23f5b9c391448c4ca84

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+  }
+
+function writeCards(array, event) {
+    let thankYouCards = []
+    for (let i=0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+    while (num >= 0) {
+    console.log(num);
+    num--;
+    }
+}
